6e4396f187b08883bb390f6f501cbf50e93f35e4
CodeShakingSheep <19874562+CodeShakingSheep@users.noreply.github.com>
2026-05-22 13:14:21 -0600

    Revert "Exec artisan command as app"
    This reverts commit ab27f78aabe9f2c0978dcf069862d6ddae36fbef.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -2,5 +2,5 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 exec_artisan() {
-  (cd "$install_dir" && ynh_exec_as_app php$php_version artisan $@ --quiet --no-interaction)
+  (cd "$install_dir" && php$php_version artisan $@ --quiet --no-interaction)
 }
