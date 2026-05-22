13038cfee5cb145a3be708d85088e804d46e805c
CodeShakingSheep <19874562+CodeShakingSheep@users.noreply.github.com>
2026-05-22 13:14:43 -0600

    Revert "Attempt to fix artisan call"
    This reverts commit 807d79653f2bb920250bcc8a9c170e018ea1cb49.

--- scripts/_common.sh
+++
@@ -1,6 +0,0 @@
-#=================================================
-# COMMON VARIABLES AND CUSTOM HELPERS
-#=================================================
-exec_artisan() {
-  (cd "$install_dir" && php$php_version artisan $@ --quiet --no-interaction)
-}
